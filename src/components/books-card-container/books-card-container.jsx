8a1d1f5292e543ffe36be5471f0f9b7cf3fa3ae3
magdy74 <magdyraffet74@gmail.com>
2024-01-24 20:43:47 +0200

    modified card component and complete pagination

--- src/components/books-card-container/books-card-container.jsx
+++ src/components/books-card-container/books-card-container.jsx
@@ -3,12 +3,21 @@ import Card from "../card/card.component";
 import Books from "../../data/data";
 import './books-card-container.scss';
 
-const BooksCardContainer = () => (
-    <div className="books-card-container">
-            {Books.map(({id, ...rest}) => (
-                <Card key={id} {...rest} booksCard={true}/>
-                ))}
-    </div>
-);
+
+
+const BooksCardContainer = ({pagenumber}) => {
+    // from, to, filter() 1*6 
+    const pageNumber= pagenumber;
+    const itemNumbers= 6;
+    const dividedBooks = Books.filter((item, index) => (pageNumber*itemNumbers-6) <= index && index < (pageNumber*itemNumbers));
+    // n to match list with page
+    return(
+        <div className="books-card-container">
+                {dividedBooks.map(({id, ...rest}) => (
+                    <Card key={id} {...rest} booksCard={true}/>
+                    ))}
+        </div>
+    )
+};
 
 export default BooksCardContainer;
--- src/pages/books-page/books.page.jsx
+++ src/pages/books-page/books.page.jsx
@@ -3,17 +3,23 @@ import BooksCardContainer from "../../components/books-card-container/books-card
 import Pagination from '@mui/material/Pagination';
 import './books.page.scss';
 import FilterContainer from "../../components/filter-container/filter-container";
+import Books from "../../data/data";
 
-const BooksPage = () => (
+const BooksPage = () => {
+    const [page, setPage] = React.useState(1);
+    const handleChange = (e,value) => {
+        setPage(value);
+    } 
+    return(
     <div className="books-page">
         <div className="container">
             <div className="filter-books-container">
                 <FilterContainer/>
-                <BooksCardContainer/>
+                <BooksCardContainer pagenumber={page}/>
             </div>
-            <Pagination className='pagination' count={5}/>
+            <Pagination className='pagination' count={Math.ceil(Books.length/6)} page={page} onChange={handleChange}/>
         </div>
     </div>
-);
+)};
 
 export default BooksPage;
